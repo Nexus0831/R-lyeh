1fcf4a73afc548acc77be300ac4f81f9ab39c485
Nexus0831 <pride0831@gmail.com>
2018-08-16 17:09:09 +0900

    feat: Listコンポーネント追加

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -7,17 +7,28 @@ import { bindActionCreators } from 'redux';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
+// Material-UI
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+// Material- Icon
+import MenuIcon from '@material-ui/icons/Menu';
+import BarChartIcon from '@material-ui/icons/BarChart';
 
 import * as actions from './../../actions/app';
 import Routes from '../Routes';
+
 import { theme } from '../../assets/theme';
+
+import { drawerTitle } from '../../../Datas/titleData';
 
 
 class App extends React.Component {
@@ -53,7 +64,14 @@ class App extends React.Component {
               </Toolbar>
             </AppBar>
             <Drawer open={this.props.app.isOpen} onClose={this.props.drawerClose}>
-              <p>Drawer</p>
+              <List>
+                <ListItem button>
+                  <ListItemIcon>
+                    <BarChartIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Chart" />
+                </ListItem>
+              </List>
             </Drawer>
             <Routes
               {...this.props}
